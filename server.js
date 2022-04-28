dc5f46210d437bf16c31444a5ecff0c105095b92
rkamta <37015885+phantomkam@users.noreply.github.com>
2022-04-28 17:17:14 +0200

    package body-parser

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+const bodyParser = require('body-parser');
 const authRoute = require('./routes/auth');
 const noteRoute = require('./routes/note.routes.js');
 const dbURI = "mongodb+srv://fais:0501484127@cluster0.sunpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
@@ -14,7 +15,7 @@ const db = mongoose.connection
 db.on("error", (err) => { console.error(err) })
 db.once("open", () => { console.log("DB started successfully") })
 
-app.use(express.json());
-// app.use('/api/auth', authRoute);
-// app.use('/api', noteRoute);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use('/api/auth', authRoute);
+app.use('/api', noteRoute);
 app.listen(3000, () => { console.log("Server started: 3000") })
