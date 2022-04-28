c744a82d528e961df379634ebe394c55daf6bdfa
rkamta <37015885+phantomkam@users.noreply.github.com>
2022-04-28 15:58:36 +0200

    sss

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
-// const authRoute = require('./routes/auth');
-// const noteRoute = require('./routes/note.routes.js');
+const authRoute = require('./routes/auth');
+const noteRoute = require('./routes/note.routes.js');
 const dbURI = "mongodb+srv://fais:0501484127@cluster0.sunpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(dbURI, { 
@@ -14,7 +14,7 @@ const db = mongoose.connection
 db.on("error", (err) => { console.error(err) })
 db.once("open", () => { console.log("DB started successfully") })
 
-// app.use(express.json());
-// app.use('/api/auth', authRoute);
-// app.use('/api', noteRoute);
+app.use(express.json());
+app.use('/api/auth', authRoute);
+app.use('/api', noteRoute);
 app.listen(3000, () => { console.log("Server started: 3000") })
