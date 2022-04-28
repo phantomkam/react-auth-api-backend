fb0219fa295a8f068d9edc0c40359a639c40026b
rkamta <37015885+phantomkam@users.noreply.github.com>
2022-04-28 10:54:42 +0200

    a

--- index.js
+++ index.js
@@ -4,13 +4,7 @@ const app = express();
 const authRoute = require('./routes/auth');
 const noteRoute = require('./routes/note.routes.js');
 
-const MONGO_USERNAME = 'doapps-124630fd-2093-429f-a075-52c54f1ad586';
-const MONGO_PASSWORD = '9iA521TR83ESh4O6';
-const MONGO_HOSTNAME = 'db-mongodb-nyc3-21958-b7113ed8.mongo.ondigitalocean.com';
-const MONGO_PORT = '27017';
-const MONGO_DB = 'react-fais';
-
-const dbURI = `mongodb+srv://doapps-124630fd-2093-429f-a075-52c54f1ad586:9iA521TR83ESh4O6@db-mongodb-nyc3-21958-b7113ed8.mongo.ondigitalocean.com/test?retryWrites=true&w=majority`;
+const dbURI = "mongodb://localhost:27017/pies";
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
