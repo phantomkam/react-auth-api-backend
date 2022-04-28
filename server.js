721a4da0d9242f9dd8f63965f00466412d83a8f5
rkamta <37015885+phantomkam@users.noreply.github.com>
2022-04-28 15:35:05 +0200

    s

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const authRoute = require('./routes/auth');
 const noteRoute = require('./routes/note.routes.js');
 const dbURI = "mongodb+srv://fais:0501484127@cluster0.sunpt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
-mongoose.connect(dbURI, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true
-})
+// mongoose.connect(dbURI, { 
+//     useNewUrlParser: true, 
+//     useUnifiedTopology: true
+// })
 
-const db = mongoose.connection
+// const db = mongoose.connection
 db.on("error", (err) => { console.error(err) })
 db.once("open", () => { console.log("DB started successfully") })
 
