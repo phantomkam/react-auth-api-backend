12067ab86d83de139165eb4effc972c3202ad99d
rkamta <37015885+phantomkam@users.noreply.github.com>
2022-04-28 09:48:54 +0200

    db

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const app = express();
 const authRoute = require('./routes/auth');
 const noteRoute = require('./routes/note.routes.js');
 
-const MONGO_USERNAME = 'doadmin';
+const MONGO_USERNAME = 'doapps-124630fd-2093-429f-a075-52c54f1ad586';
 const MONGO_PASSWORD = '9iA521TR83ESh4O6';
 const MONGO_HOSTNAME = 'db-mongodb-nyc3-21958-b7113ed8.mongo.ondigitalocean.com';
 const MONGO_PORT = '27017';
-const MONGO_DB = 'admin';
+const MONGO_DB = 'react-fais';
 
-const dbURI = `mongodb://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}?authSource=admin`;
+const dbURI = `mongodb+srv://${MONGO_USERNAME}:${MONGO_PASSWORD}@${MONGO_HOSTNAME}:${MONGO_PORT}/${MONGO_DB}`;
 app.use(express.json());
 
 app.use('/api/auth', authRoute);
